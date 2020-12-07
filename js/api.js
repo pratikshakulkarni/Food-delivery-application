4560ad4317e27a44a960404a30235ea369cf6c89
Pratiksha Kulkarni <kulkarnipratiksha@gmail.com>
2020-12-07 22:20:39 +0530

    commit

---
+++ js/api.js
@@ -0,0 +1,11 @@
+function updateSelect(){
+    
+}
+
+document.getElementById("search").addEventListener("click",function(){
+    var search = document.getElementById("searchInput").value;
+    console.log(search);
+});
+
+
+
---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Login Validation
+const lNumber = document.getElementById("lNumber");
+
+//Register validation
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const pcPassword = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const mobileNumber = document.getElementById("mobileNumber");
+
+//ALl or any empties
+
+
+//password length 
+
+//mobile number length
